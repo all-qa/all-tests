56cbc20abe480815c8eeef4c26eba0f31aa906c9
svitskyi <oleg.svitskyi@protonmail.ch>
2022-10-01 23:48:44 +0200

    adding init page handling

--- src/main/java/com/test/pages/GooglePage.java
+++ src/main/java/com/test/pages/GooglePage.java
@@ -6,8 +6,11 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 @Slf4j
 public class GooglePage extends BasePageObject {
@@ -24,6 +27,16 @@ public class GooglePage extends BasePageObject {
     public GooglePage openPage() {
         Capabilities capabilities = ((RemoteWebDriver) getWebDriver()).getCapabilities();
         getWebDriver().get(url);
+        String acceptButtonColor = "#4285f4";
+        List<WebElement> elements = getWebDriver().findElements(By.xpath("//div[contains(text(), 'Alle akzeptieren')]"));
+        for (WebElement element: elements) {
+            String color = element.getCssValue("background-color");
+            if (Color.fromString(color).asHex().equals(acceptButtonColor)) {
+                element.click();
+                break;
+            }
+        }
+        elements.get(elements.size()-2).click();
         getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
         log.info("Opening google page {} in the following browser: {} and {} language", getWebDriver().getCurrentUrl(),capabilities.getBrowserName(), getLanguage());
         return this;
